45501b8dd84fa4ca497935d4c538295c227a58cd
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2025-03-08 11:27:13 +0530

    dev and prod mode

--- src/main/java/com/bridgelabz/Employee_Payroll/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/Employee_Payroll/EmployeePayrollApplication.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.Employee_Payroll;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-	}
 
+
+		ConfigurableApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+	}
 }
